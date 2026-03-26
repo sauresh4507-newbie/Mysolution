094f316d2e2ce3f9a0bb8cd7174497d6b944a929
sauresh4507-newbie <saureshsdbsc24@rvu.edu.in>
2026-03-26 10:00:46 +0530

    Initial commit: add sumofdigits.py

---
+++ sumofdigits.py
@@ -0,0 +1,9 @@
+def sum_of_digits(n):
+    n = abs(n) # handle negatives 
+    total = 0 
+    while n > 0: 
+        total += n %10 
+        n //= 10
+    return total 
+
+print(sum_of_digits(123455)) 
